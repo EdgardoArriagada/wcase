c63923b3ab4815bd7550d4cbe80ef6dff943ac2b
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-09-17 13:54:09 -0300

    wip get_case function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod args;
 use args::Args;
 
+extern crate regex;
+use regex::Regex;
+
 use clap::Parser;
 
 fn main() {
@@ -30,6 +33,56 @@ fn main() {
     println!("{}", result);
 }
 
+fn get_case(word: &str) -> String {
+    let mut result = String::new();
+    if word.contains(' ') {
+        return "none".into();
+    }
+
+    if !word.contains('-') || !word.contains('_') {
+        if word.to_lowercase() == word {
+            return "flat".into();
+        } else if word.to_uppercase() == word {
+            return "upper".into();
+        }
+
+        let camel_case = Regex::new(r"^[a-z]+(?:[A-Z][a-z]+)*$").unwrap();
+        if camel_case.is_match(word) {
+            return "camel".into();
+        }
+
+        let pascal_case = Regex::new(r"^[A-Z][a-z]+(?:[A-Z][a-z]+)*$").unwrap();
+        if pascal_case.is_match(word) {
+            return "pascal".into();
+        }
+    }
+
+    if word.contains('-') {
+        result.push_str("kebab");
+    }
+
+    return "none".into();
+}
+
 fn flat_case(word: &str) -> String {
     word.to_lowercase()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_flat_case() {
+        assert_eq!(flat_case("Hello World"), "hello world");
+    }
+
+    #[test]
+    fn test_get_case() {
+        assert_eq!(get_case("helloworld"), "flat");
+        assert_eq!(get_case("HELLOWORLD"), "upper");
+        assert_eq!(get_case("hello world"), "none");
+        assert_eq!(get_case("HelloWorld"), "pascal");
+        assert_eq!(get_case("helloWorld"), "camel");
+    }
 }
