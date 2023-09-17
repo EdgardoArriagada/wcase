5ee1ff4f8932b899390079a0993011cad9bcf2ec
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-09-17 13:04:22 -0300

    match args

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,25 @@ use clap::Parser;
 
 fn main() {
     let args = Args::parse();
-    println!("{:?}", args);
+
+    match args {
+        Args { flat: true, .. } => println!("flat case"),
+        Args { upper: true, .. } => println!("upper case"),
+        Args { camel: true, .. } => println!("camel case"),
+        Args { pascal: true, .. } => println!("pascal case"),
+        Args { snake: true, .. } => println!("snake case"),
+        Args { all_caps: true, .. } => println!("all_caps case"),
+        Args {
+            camel_snake: true, ..
+        } => println!("camel_snake case"),
+        Args {
+            pascal_snake: true, ..
+        } => println!("pascal_snake case"),
+        Args { kebab: true, .. } => println!("kebab case"),
+        Args { train: true, .. } => println!("train case"),
+        Args {
+            http_header: true, ..
+        } => println!("http_header case"),
+        _ => println!("no match"),
+    }
 }
